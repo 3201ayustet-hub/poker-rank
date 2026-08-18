9b6ca5a41ea2cd2b53cd5a6c461c6d986c924189
3201ayustet-hub <3201ayustet@gmail.com>
2026-08-18 22:30:17 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.POKER_CONFIG = Object.freeze({
-  version: '1.3',
-  supabaseUrl: 'https://uczkqklqdbzkxerboatx.supabase.co',
-  supabasePublishableKey: 'sb_publishable_3GgBI0FRcfcA7YaN0VWu7A_DpU1jI_k',
-  cloudEnabled: true
-});
