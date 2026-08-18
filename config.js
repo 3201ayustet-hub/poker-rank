c32c72caa30a6f5d3fb44c0996a508865afa3cad
3201ayustet-hub <3201ayustet@gmail.com>
2026-08-18 22:59:10 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.POKER_CONFIG = Object.freeze({
+  version: '1.5',
+  supabaseUrl: 'https://uczkqklqdbzkxerboatx.supabase.co',
+  supabasePublishableKey: 'sb_publishable_3GgBI0FRcfcA7YaN0VWu7A_DpU1jI_k',
+  cloudEnabled: true
+});
